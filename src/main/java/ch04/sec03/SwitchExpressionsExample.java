6b648a98b64200dcef01f0818ab48494c01141d0
noah <jeonghogim318@gmail.com>
2023-10-04 13:52:27 +0900

    ch04sec04 for문

--- src/main/java/ch04/sec03/SwitchExpressionsExample.java
+++ src/main/java/ch04/sec03/SwitchExpressionsExample.java
@@ -11,11 +11,9 @@ public class SwitchExpressionsExample {
         switch(grade){
             case 'A', 'a' ->{
                 System.out.println("우수 회원입니다.");
-
             }
             case 'B', 'b' -> {
                 System.out.println("일반 회원입니다.");
-
             }
             default -> {
                 System.out.println("손님입니다.");
