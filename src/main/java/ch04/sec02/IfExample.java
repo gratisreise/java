5a81bf3387d3c3ab64c4d1e03f8e657ba36088a6
noah <jeonghogim318@gmail.com>
2023-10-04 13:38:06 +0900

    ch04sec03 switch문

--- src/main/java/ch04/sec02/IfExample.java
+++ src/main/java/ch04/sec02/IfExample.java
@@ -11,7 +11,7 @@ public class IfExample {
             System.out.println("점수가 90보다 큽니다.");
             System.out.println("등급은 A입니다.");
         }
-        
+
         if(score < 90){
             System.out.println("점수가 90보다 작습니다.");
             System.out.println("등급은 B입니다.");
---
+++ src/main/java/ch04/sec03/SwitchCharExample.java
@@ -0,0 +1,24 @@
+package ch04.sec03;
+
+import java.util.Scanner;
+
+public class SwitchCharExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char grade = sc.next().charAt(0);
+
+        switch(grade) {
+            case 'A':
+            case 'a':
+                System.out.println("우수 회원입니다.");
+                break;
+            case 'B':
+            case 'b':
+                System.out.println("일반 회원입니다.");
+                break;
+            default:
+                System.out.println("손님입니다.");
+        }
+    }
+}
