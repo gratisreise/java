dee8723aa4568bdf54b69b8ca334704daa33cadf
noah <jeonghogim318@gmail.com>
2023-10-06 17:26:29 +0900

    ch07sec07 타입변환

--- src/main/java/ch07/sec03/exam01/SmartPhoneExample.java
+++ src/main/java/ch07/sec03/exam01/SmartPhoneExample.java
@@ -5,7 +5,7 @@ import ch07.sec02.SmartPhone;
 public class SmartPhoneExample {
     public static void main(String[] args) {
         //SmartPhone 객체 생성
-        SmartPhone myPhone = new SmartPhone();
+        SmartPhone myPhone = new SmartPhone("스마트폰", "빨강");
 
         //Phone으로부터 상속 받은 필드 읽기
         System.out.println("모델: " + myPhone.model);
