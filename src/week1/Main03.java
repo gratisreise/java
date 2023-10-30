e15c6494da74180dc44a2c708f0169b997100cd3
noah <jeonghogim318@gmail.com>
2023-10-30 09:47:55 +0900

    10988 펠린드롬인지 확인하기2

--- src/week1/Main03.java
+++ src/week1/Main03.java
@@ -1,2 +1,29 @@
-package week1;public class Main03 {
+package week1;
+import java.util.*;
+public class Main03 {
+    static int cnt[] = new int[104];
+    static int ret;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        //시간은 이상 미만으로
+        for(int i = 0; i < 3; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            for(int  j = a; j < b; j++){
+                cnt[j]++;
+            }
+        }
+
+        for(int i = 0; i < 100; i++){
+            if(cnt[i] == 1) ret += A;
+            else if(cnt[i] == 2) ret += B * 2;
+            else if(cnt[i] == 3) ret += C * 3;
+        }
+
+        System.out.println(ret);
+    }
 }
--- src/week1/Main04.java
+++ src/week1/Main04.java
@@ -1,2 +1,15 @@
-package week1;public class Main04 {
+package week1;
+import java.util.*;
+
+public class Main04 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        // reverse메소드 사용하기 위해 StringBuilder로 감싸기
+        StringBuilder temp = new StringBuilder(s);
+        // 앞뒤 전환한 것을 스트링으로 변환
+        String ss = temp.reverse().toString();
+        if(s.equals(ss)) System.out.println(1);
+        else System.out.println(0);
+    }
 }
