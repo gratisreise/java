b6d3228ab8fe12cb1620450d1be4e0d650e2e18e
noah <jeonghogim318@gmail.com>
2023-10-31 09:28:11 +0900

    1940 주몽

---
+++ src/week1/Main12.java
@@ -0,0 +1,32 @@
+package week1;
+
+import java.util.*;
+
+public class Main12 {
+    static int[] a  = new int[100004];
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+        }
+
+        if(m > 200000){
+            System.out.println(0);
+            return;
+        }
+
+        int ret = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(a[i] + a[j] == m){
+                    ret++;
+                }
+            }
+        }
+        System.out.println(ret);
+    }
+}
